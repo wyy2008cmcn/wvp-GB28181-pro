a5a82e1735457c4ab350e456f5f0f36a14f6294a
昼颜 <zhouyan@wacai.com>
2022-08-23 11:52:07 +0800

    修改布局

--- web_src/src/components/Login.vue
+++ web_src/src/components/Login.vue
@@ -3,7 +3,7 @@
   <div class="limiter">
     <div class="container-login100">
       <div class="wrap-login100">
-					<span class="login100-form-title p-b-26">WVP视频平台</span>
+					<span class="login100-form-title p-b-26">ZTO-嘉兴监控</span>
           <span class="login100-form-title p-b-48">
 						<i class="fa fa-video-camera"></i>
 					</span>
